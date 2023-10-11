3633c4a2151b7b688a3c63935422fc7ad11f0ed2
JoInHyeok <ss112d86@gmail.com>
2023-10-11 10:43:11 +0900

    prometheus, grafana boot monitoring code

---
+++ monitoringboot/src/main/java/com/example/monitoringboot/MonitoringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.monitoringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringbootApplication.class, args);
+	}
+
+}
---
+++ monitoringboot/src/test/java/com/example/monitoringboot/MonitoringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monitoringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
